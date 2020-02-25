3b27ba945e0d547983fd24773b624d53fd75b72a
Omar Shafei <omarshafhy.os@gmail.com>
2020-02-25 07:56:07 +0200

    getting the data by making HTTP request instead of the hardcoding of the JSON file

--- app/src/main/java/com/example/earthquakereport/MainActivity.java
+++ app/src/main/java/com/example/earthquakereport/MainActivity.java
@@ -11,10 +11,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    String strJson;
     private RecyclerView recyclerView;
     private CustomAdapter adapter;
 
@@ -23,15 +31,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = findViewById(R.id.recycler_view);
-        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        //HTTP request
+        OkHttpClient client = new OkHttpClient();
 
-        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
+        String url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    strJson = response.body().string();
+
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes(strJson);
+                            recyclerView = findViewById(R.id.recycler_view);
+                            recyclerView.addItemDecoration(new DividerItemDecoration(getApplicationContext(), LinearLayoutManager.VERTICAL));
+
+                            adapter = new CustomAdapter(getApplicationContext(), earthquakes);
+                            recyclerView.setAdapter(adapter);
+                            recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                        }
+                    });
+                }
+            }
+        });
 
 
-        adapter = new CustomAdapter(getApplicationContext(), earthquakes);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
 
     }
 }
